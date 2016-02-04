fefd92d470c71dfd1089aaf005b3094ca76609a4
rc3avn <r.celaya@activevideo.com>
2016-02-04 13:50:39 -0800

    added new types from documentation

--- src/services/google-maps-types.ts
+++ src/services/google-maps-types.ts
@@ -9,6 +9,7 @@ export interface GoogleMap {
   setCenter(latLng: LatLng | LatLngLiteral): void;
   getZoom(): number;
   setOptions(options: MapOptions): void;
+  setMapTypeId(mapTypeId: string): void;
 }
 
 export interface LatLng {
@@ -18,12 +19,14 @@ export interface LatLng {
 }
 
 export interface Marker {
+  icon: Icon;
   constructor(options?: MarkerOptions): void;
   setMap(map: GoogleMap): void;
   setPosition(latLng: LatLng | LatLngLiteral): void;
   setTitle(title: string): void;
   setLabel(label: string | MarkerLabel): void;
   getLabel(): MarkerLabel;
+  setIcon(icon: Icon): void;
   addListener(eventType: string, fn: Function): void;
 }
 
@@ -47,8 +50,56 @@ export interface LatLngLiteral {
   lng: number;
 }
 
+export interface Point {
+  x: number;
+  y: number;
+}
+export interface Size {
+  width: number;
+  height: number;
+}
+
+export interface Icon {
+  anchor: Point;
+  labelOrigin: Point;
+  origin: Point;
+  scaledSize: Size;
+  size: Size;
+  url: string;
+}
+
 export interface MapOptions {
+  backgroundColor?: string;
   center?: LatLng | LatLngLiteral;
-  zoom?: number;
+  disableDefaultUI?: boolean;
   disableDoubleClickZoom?: boolean;
+  draggable?: boolean;
+  draggableCursor?: boolean;
+  draggingCursor?: boolean;
+  heading?: number;
+  keyboardShortcuts?: boolean;
+  mapMaker?: boolean;
+  mapTypeControl?: boolean;
+  mapTypeControlOptions?: boolean;
+  //mapTypeId?: MapTypeId;
+  maxZoom?: number;
+  minZoom?: number;
+  noClear?: boolean;
+  overviewMapControl?: boolean;
+  //overviewMapControlOptions?: OverviewMapControlOptions;
+  panControl?: boolean;
+  //panControlOptions?: PanControlOptions;
+  rotateControl?: boolean;
+  rotateControlOptions?: boolean;
+  scaleControl?: boolean;
+  //scaleControlOptions?: ScaleControlOptions;
+  scrollwheel?: boolean;
+  //streetView?: StreetViewPanorama;
+  streetViewControl?: boolean;
+  //streetViewControlOptions?: streetViewControlOptions;
+  //styles?: MapTypeStyle[];
+  tilt?: number;
+  zoom?: number;
+  zoomControl?: boolean;
+  //zoomControlOptions?:  ZoomControlOptions;
 }
